78481da49dfbf19a00188ee8cc5cc8f787805d93
ZachAnaz <zachroy04@yahoo.com>
2023-02-27 14:42:05 +0800

    intro to JS

---
+++ JavaScript from Frontend to Backend/index.js
@@ -0,0 +1,2 @@
+// alert('hai')
+console.log("This is a warning message displayed by JavaScript");
---
+++ JavaScript from Frontend to Backend/testnode.js
@@ -0,0 +1,8 @@
+var a=22;
+
+console.log("a = " + a);
+if (a==12) {
+    console.log("correct")   
+} else {
+    console.log("not correct") 
+};
